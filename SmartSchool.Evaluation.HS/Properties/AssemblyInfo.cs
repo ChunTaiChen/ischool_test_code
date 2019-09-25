160d28c7765567d19617471b6c497fa8fca641f1
chuntaichen <ct.chen@ischool.com.tw>
2019-09-25 16:08:55 +0800

    修正產生課程規劃表問題。

--- SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
+++ SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      組建編號
 //      修訂編號
 //
-[assembly: AssemblyVersion("1.0.7.0015")]
-[assembly: AssemblyFileVersion("1.0.7.0015")]
+[assembly: AssemblyVersion("1.0.7.0016")]
+[assembly: AssemblyFileVersion("1.0.7.0016")]
